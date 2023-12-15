223041cee16b2564e1388408d7e2109b0a2b66c3
CaleBaguley <jb14389@bristol.ac.uk>
2023-12-15 16:36:35 +0000

    Added function to calculate if an index satisfies fizz buzz and set fizz_buzz_access to correctly access array.

--- manipulations/library.py
+++ manipulations/library.py
@@ -2,6 +2,17 @@ from typing import TypeVar
 
 T = TypeVar("T")
 
+
+def _is_fizz_buzz(i: int) -> bool:
+    """
+    Takes integer and returns true if it is ether divisable by 3 or 5 but not both
+    """
+    is_div_3 = i%3 == 0
+    is_div_5 = i%5 == 0
+
+    return (int(is_div_3) + int(is_div_5)) == 1
+
+
 def fizz_buzz_access(items: list[T]) -> list[T]:
     """
     Access the list according to the fizz-buzz pattern, that is, elements
@@ -10,4 +21,4 @@ def fizz_buzz_access(items: list[T]) -> list[T]:
     - is divisible by 3 or 5
     - not divisible by 3 and 5
     """
-    return items
+    return [items[i] for i in range(len(items)) if _is_fizz_buzz(i)]
